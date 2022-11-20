dea8a81d41300fc20451802b141bcc72674fcbc5
olegblyzniuk <mettal062@gmail.com>
2022-11-20 14:35:40 +0200

    most

--- js/index.js
+++ js/index.js
@@ -25,3 +25,25 @@ function navLinkClick() {
     navToggler.click();
   }
 }
+
+$( function() {
+  $( "#accordion" ).accordion();
+} );
+$('.owl-carousel').owlCarousel({
+  loop:true,
+  margin:0,
+  nav:true,
+  navText: [ '', '' ],
+  autoplay: false,
+  autoplayTimeout:2000,
+  items:1,
+  // responsive:{
+  //     0:{
+  //         items:1
+  //     },
+
+  //     1000:{
+  //         items:5
+  //     }
+  // }
+});
